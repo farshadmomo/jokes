c459f3f8969da82d4f1c4c1017e44497166cd7db
farshadmomo <momtazfarshad@gmail.com>
2023-08-26 22:36:37 +0330

    first joke commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import Jokes from "./components/jokes";
+import jokesData from "./jokesData"
 export default function App() {
+    const jokeElements = jokesData.map((joke) => {
+        return <Jokes
+        setup={joke.setup}
+        punchline={joke.punchline}
+        />
+    })
     return (
         <div>
-            <Jokes
-                setup="Why do fathers take an extra pair of socks when they go golfing?"
-                punchline="In case they get a hole in one!"/>
-            <Jokes
-                setup="What do you call a factory that makes okay products?"
-                punchline="A satisfactory."/>
-            <Jokes
-                setup="What did the janitor say when he jumped out of the closet?"
-                punchline="Supplies!"/>
-            <Jokes
-                setup="What did the ocean say to the beach?"
-                punchline="Nothing, it just waved."/>
-            <Jokes
-                setup="Why do seagulls fly over the ocean?"
-                punchline="Because if they flew over the bay, we'd call them bagels."/>
-
+            {jokeElements}
         </div>
     );
 }
---
+++ src/jokesData.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        setup: "I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she opens it."
+    },
+    {
+        setup: "How did the hacker escape the police?",
+        punchline: "He just ransomware!"
+    },
+    {
+        setup: "Why don't pirates travel on mountain roads?",
+        punchline: "Scurvy."
+    },
+    {
+        setup: "Why do bees stay in the hive in the winter?",
+        punchline: "Swarm."
+    },
+    {
+        setup: "What's the best thing about Switzerland?",
+        punchline: "I don't know, but the flag is a big plus!"
+    },
+    {
+        setup: "Why do seagulls fly over the ocean?",
+        punchline: "Because if they flew over the bay, we'd call them bagels."
+    },
+    {
+        setup: "Why do fathers take an extra pair of socks when they go golfing?",
+        punchline: "In case they get a hole in one!"
+    },
+    {
+        setup: "What do you call a factory that makes okay products?",
+        punchline: "A satisfactory."
+    },
+    {
+        setup: "What did the janitor say when he jumped out of the closet?",
+        punchline: "Supplies!"
+    },
+    {
+        setup:"What did the ocean say to the beach?",
+        punchline:"Nothing, it just waved."
+    }
+]
