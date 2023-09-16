17b55c7d4f2cb8d61e6be126493b4ce9b3bebb50
farshadmomo <momtazfarshad@gmail.com>
2023-09-16 11:49:59 +0330

    punchline

--- src/components/jokes.js
+++ src/components/jokes.js
@@ -1,8 +1,16 @@
+import React from "react";
+
 export default function Jokes(props) {
+    const [isShown , setIsShown] = React.useState(false)
+    function handleToggle() {
+        return setIsShown(prevState => !prevState)
+    }
     return (
         <div className={'jokeContainer'}>
             {props.setup && <h3>{props.setup}</h3>}
-            <p>{props.punchline}</p>
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={handleToggle}>Show Punchline</button>
         </div>
+
     )
 }
